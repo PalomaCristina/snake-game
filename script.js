44107f73eadffaad0c6bc641f18e9b6d2edd4168
Paloma <paloma1899@gmail.com>
2021-02-03 22:14:48 -0300

    definir aleatoriedade da frutinha e aumentar o tamanho da cobrinha quando comer

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ snake[0] ={
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 
 function criarBG(){
     context.fillStyle = '#51DCA3';
@@ -16,9 +21,15 @@ function criarBG(){
 
 function criarCobrinha (){
     for(i = 0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = '#0A3E89';
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = '#7C074B';
+    context.fillRect(food.x, food.y, box, box);
+
 }
 
 document.addEventListener('keydown', update);
@@ -33,14 +44,15 @@ function update(event){
 
 function iniciarJogo() {
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box; 
 
 
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x; //define o ponto inicial da cobrinha
     let snakeY = snake[0].y; //define o ponto inicial da cobrinha
