53d5def0de070f87ce96ded8a802c6ba8be3b696
Paloma <paloma1899@gmail.com>
2021-02-03 21:03:13 -0300

    impedir que a cobrinha suma e definir direcoes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let canvas = document.getElementById("snake"); //cria o elemento
 let context = canvas.getContext("2d"); 
 let box = 32;
 let snake = []; //cria cobrinha como array
+
 snake[0] ={
     x: 8 * box,
     y: 8 * box
@@ -9,18 +10,35 @@ snake[0] ={
 let direction = "right";
 
 function criarBG(){
-    context.fillStyle =  '#51DCA3';
-    context.fillRect(0, 0, 16*box, 16*box); //desenha o retângulo x e y, largura e altura
+    context.fillStyle = '#51DCA3';
+    context.fillRect(0, 0, 16 * box, 16 * box); //desenha o retângulo x e y, largura e altura
 }
 
-function criarCobrinha() {
+function criarCobrinha (){
     for(i = 0; i < snake.length; i++){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
+
 function iniciarJogo() {
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+
+
     criarBG();
     criarCobrinha();
 
@@ -30,7 +48,7 @@ function iniciarJogo() {
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
-    if(direction == "down") snakey += box;
+    if(direction == "down") snakeY += box;
 
     snake.pop();
 
@@ -39,7 +57,7 @@ function iniciarJogo() {
         y: snakeY
     }
 
-    snake.unshift(newHead);
+    snake.unshift(newHead); // primeiro quadrado da cobrinha
 
 }
 
